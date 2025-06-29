4f9beec33e20d26a15a4d047a2c43eaa175a4a7d
Andrew <trotsiuk.andriy@lll.kpi.ua>
2025-06-29 16:28:06 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+    const new_arr = []
+    for (let number of numbers) {
+        if (number > value) {
+            new_arr.push(number)
+        }
+    }
+    return new_arr
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
